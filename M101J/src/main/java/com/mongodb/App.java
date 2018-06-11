00cb2319c3a3de960172e455e298646e2f9270f1
Felipe Torrejon <felipetorrejon@live.cl>
2018-06-11 19:12:57 -0400

    The MongoDB Java Driver

--- M101J/src/main/java/com/mongodb/App.java
+++ M101J/src/main/java/com/mongodb/App.java
@@ -1,13 +1,20 @@
 package com.mongodb;
 
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+  public static void main( String[] args ) {
+    MongoClientOptions options = MongoClientOptions.builder().connectionsPerHost(100).build();
+    MongoClient client = new MongoClient(new ServerAddress(), options);
+    
+    MongoDatabase db = client.getDatabase("test");
+    
+    MongoCollection<Document> coll= db.getCollection("test");
+  }
 }
